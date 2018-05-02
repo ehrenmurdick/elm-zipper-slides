e4de0fb265aaae3f4e3189e0f69944371f543467
Ehren Murdick <ehren.murdick@gmail.com>
2018-05-02 15:09:14 -0700

    first ruby section done maybe

--- code/edit_one_item.rb
+++ code/edit_one_item.rb
@@ -1,14 +1,16 @@
-sildes = [
-  { title: "first" },
-  { title: "second" },
-  ...
-]
-current_index = 4
+def init
+  @sildes = [
+    { title: "first" },
+    { title: "second" },
+    ...
+  ]
+  @current_index = 4
+end
 
 def next
-  current_index = current_index + 1
+  @current_index = @current_index + 1
 end
 
 def update_current_slide_title(new_title)
-  slides[current_index]["title"] = new_title
+  @slides[@current_index]["title"] = new_title
 end
