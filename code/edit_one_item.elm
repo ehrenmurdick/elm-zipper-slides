112659b316a39a86422c7b66f84d4fbd2950a27e
Ehren Murdick <ehren.murdick@gmail.com>
2018-05-02 15:00:44 -0700

    added intro slide

--- code/edit_one_item.elm
+++ code/edit_one_item.elm
@@ -1,9 +1,24 @@
 module Main exposing (..)
 
 
+type alias Slide =
+    { title : String
+    }
+
+
 type alias Model =
     { currentIndex : Int
     , slides : List Slide
+    }
+
+
+init : Model
+init =
+    { currentIndex = 0
+    , slides =
+        [ { title = "first" }
+        , { title = "second" }
+        ]
     }
 
 
